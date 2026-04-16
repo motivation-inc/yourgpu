5d71e3bfd47f055b9352062162cee1de5d019f6d
ktechhydle <kellerhydle@gmail.com>
2026-04-15 20:43:27 -0600

    Window Updates
    -add resize function for resizing the surface configuration

--- src/window.rs
+++ src/window.rs
@@ -5,3 +5,12 @@ pub struct WindowSurface<'a> {
     pub(crate) surface: wgpu::Surface<'a>,
     pub(crate) config: wgpu::SurfaceConfiguration,
 }
+
+impl<'a> WindowSurface<'a> {
+    pub fn resize(&mut self, width: u32, height: u32) {
+        if width > 0 && height > 0 {
+            self.config.width = width;
+            self.config.height = height;
+        }
+    }
+}
