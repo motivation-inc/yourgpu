1bfbba8019591cdaeaf59b2cc08bb58d64d0b15d
ktechhydle <kellerhydle@gmail.com>
2026-04-17 15:37:35 -0600

    Example Updates
    -rgb triangle

--- examples/01_triangle.rs
+++ examples/01_triangle.rs
@@ -1,34 +1,38 @@
 use image::{ImageBuffer, Rgba};
 use yourgpu::{
-    BindingBuilder, BufferType, Context, TextureDimension, TextureFormat, TextureType,
-    VertexAttributeFormat, VertexLayoutBuilder,
+    BufferType, Context, TextureDimension, TextureFormat, TextureType, VertexAttributeFormat,
+    VertexLayoutBuilder,
 };
 
 fn main() {
     let mut ctx = Context::new();
     let prog = ctx.program(
         r#"
+            struct VSOut {
+                @builtin(position) position: vec4<f32>,
+                @location(0) color: vec3<f32>,
+            };
+
             @vertex
-            fn vs(@location(0) position: vec3<f32>) -> @builtin(position) vec4<f32> {
-                return vec4<f32>(position, 1.0);
+            fn vs(
+                @location(0) position: vec3<f32>,
+                @location(1) color: vec3<f32>,
+            ) -> VSOut {
+                var out: VSOut;
+                out.position = vec4<f32>(position, 1.0);
+                out.color = color;
+                return out;
             }
         "#,
         Some(
             r#"
-            struct Color {
-                value: vec4<f32>,
-            };
-
-            @group(0) @binding(0)
-            var<uniform> u_color: Color;
-
             @fragment
-            fn fs() -> @location(0) vec4<f32> {
-                return u_color.value;
+            fn fs(@location(0) color: vec3<f32>) -> @location(0) vec4<f32> {
+                return vec4<f32>(color, 1.0);
             }
         "#,
         ),
-        &[BindingBuilder::new(0).uniform("u_color", 0)],
+        &[],
     );
     let tex = ctx.texture(
         (1080, 1080, 1),
@@ -40,24 +44,27 @@ fn main() {
 
     // vertex buffer
     let vbo = ctx.buffer(
-        &[0.0_f32, 0.6, 0.0, -0.6, -0.6, 0.0, 0.6, -0.6, 0.0],
+        &[
+            // position    // color
+            0.0_f32, 0.6, 0.0, 1.0, 0.0, 0.0, // top = red
+            -0.6, -0.6, 0.0, 0.0, 1.0, 0.0, // left = green
+            0.6, -0.6, 0.0, 0.0, 0.0, 1.0, // right = blue
+        ],
         BufferType::Vertex,
     );
-
-    // uniform buffer (orange-ish)
-    let color_buffer = ctx.buffer(&[1.0_f32, 0.5, 0.0, 1.0], BufferType::Uniform);
 
     // vertex array
     let vao = ctx.vertex_array(
         &vbo,
         None,
-        VertexLayoutBuilder::new().attr(0, VertexAttributeFormat::Float32x3),
+        VertexLayoutBuilder::new()
+            .attr(0, VertexAttributeFormat::Float32x3) // position
+            .attr(1, VertexAttributeFormat::Float32x3), // color
     );
 
     // render pass
     ctx.render_texture(&prog, &tex, None, |r| {
         r.clear(0.0, 0.0, 0.0, 1.0); // black background
-        r.set_uniform("u_color", &color_buffer); // set the uniform
 
         r.draw(&vao); // draw
     });
